0a64b34a67fe41bf8d6e91691149932e8d4a2a67
Matt Fagan <matt.dev.work@gmail.com>
2022-06-14 06:31:15 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# UI Component Storybook with Testing and Accessibility
+
+## Tech Stack
+
+- [x] Storybook
+- [x] React.js
+- [x] TypeScript
+- [x] TailwindCSS
+- [x] React Testing Library, Jest, and Enzyme
+
+### Commands
+
+UI
+
+```
+yarn storybook
+```
+
+Test
+
+```
+yarn test
+```
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,45 @@
+import React, {
+  ForwardedRef,
+  forwardRef,
+  ForwardRefExoticComponent,
+  HTMLAttributes,
+  ReactNode,
+  RefAttributes,
+} from 'react';
+
+export interface Props extends HTMLAttributes<HTMLButtonElement> {
+  children: ReactNode;
+  onClick: () => void;
+  round?: boolean;
+  variant?: 'contained' | 'outlined' | 'text';
+}
+
+export const Button: ForwardRefExoticComponent<
+  Props & RefAttributes<HTMLButtonElement>
+> = forwardRef<HTMLButtonElement, Props>(
+  (
+    { children, onClick, round, variant = 'contained', ...props },
+    ref: ForwardedRef<HTMLButtonElement>
+  ): JSX.Element => {
+    const color: string = 'emerald';
+    const classes: { [key: string]: string } = {
+      contained: `bg-${color}-700 shadow-md hover:shadow-xl text-white`,
+      outlined: `border border-${color}-500 hover:bg-${color}-100`,
+      text: `hover:bg-${color}-100`,
+    };
+    return (
+      <button
+        className={`${classes[variant]}${
+          round ? ' rounded-full h-8 w-8' : ''
+        } p-2 transition ease-in-out duration-300 filter cursor-pointer flex justify-center items-center select-none focus:outline-none focus-visible`}
+        onClick={onClick}
+        ref={ref}
+        {...props}
+      >
+        {children}
+      </button>
+    );
+  }
+);
+
+export default Button;
